1339d2d1af176109ea973806569a7642e3cb2924
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2025-09-23 12:11:36 +0200

    still learning about php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 echo "Hello, world";
+$name = "Byiringiro Aime Thierry";
+$age = 50;
+echo "<br>";
+echo "My name is ".$name." and I am ".$age." years old";
+//displaying float numbers using printf
+$float = 10.44;
+echo "<br>";
+printf("the float is %.2f",$float);
+var_dump($float);
+echo gettype($float);
 ?>
