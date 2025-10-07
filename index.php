c3b6fdf57381c7a711637bdac5d38ddfc57dfc60
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2025-10-07 14:29:25 +0200

    checking grade of your score

--- index.php
+++ index.php
@@ -1,26 +1,35 @@
-<?php
-echo "Hello, world";
-$name = "Byiringiro Aime Thierry";
-$age = 50;
-echo "<br>";
-echo "My name is ".$name." and I am ".$age." years old";
-//displaying float numbers using printf
-$float = 10.44;
-echo "<br>";
-printf("the float is %.2f",$float);
-var_dump($float);
-echo gettype($float);
-$i = 0;
+<!DOCTYPE html>
+<html lang="en">
 
-for($i = 0;$i<5;$i++){
-    echo"<br>";
-    echo $i;
-}
-    echo"<br>";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-    $a = ["A",3,55,"giir"];
-    echo "<pre>";
-    // echo implode("<br>",$a);
-    echo json_encode($a);
-    echo "</pre>";
-?>
+<body>
+    <form action="#" method="post">
+        <input type="number" name="score" placeholder="enter your score">
+        <input type="submit" value="Check Score" name="submit">
+    </form>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+                $score = $_POST['score'];
+        if($score >= 90 && $score <= 100){
+                echo "<h1>A</h1>";
+        }else if($score >=80 && $score <= 89){
+                echo "<h1>B</h1>";
+        }elseif($score >= 70 && $score <= 79){
+                echo "<h1>C</h1>";
+        } elseif($score >= 60 && $score <= 69){
+                echo "<h1>D</h1>";
+        }elseif($score >= 0 && $score <= 50){
+                echo "<h1>F</h1>";
+        }else{
+                echo "invalid marks";
+        }
+                }
+    ?>
+</body>
+
+</html>
